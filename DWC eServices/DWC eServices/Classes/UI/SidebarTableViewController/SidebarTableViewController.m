3db2a571e6b46f8c10baf6d1499c12e97f96339d
Mina <mzakama@cloudconcptgroup.com>
2015-01-27 12:30:22 +0200

    SideBar

---
+++ DWC eServices/DWC eServices/Classes/UI/SidebarTableViewController/SidebarTableViewController.m
@@ -0,0 +1,66 @@
+//
+//  SidebarTableViewController.m
+//  DWC eServices
+//
+//  Created by Mina Zaklama on 1/27/15.
+//  Copyright (c) 2015 Cloud Concept. All rights reserved.
+//
+
+#import "SidebarTableViewController.h"
+
+@interface SidebarTableViewController ()
+
+@end
+
+@implementation SidebarTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    menuItems = @[@"Dashboard Cell", @"My Request Cell", @"Employees Cell", @"Services Cell", @"Company Info Cell", @"Reports Cell", @"Quick Access Cell", @"Need Help Cell", @"Logout Cell"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return menuItems.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    // This will create a "invisible" footer
+    return 0.01f;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[menuItems objectAtIndex:indexPath.row]
+                                                            forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    // [segue.identifier isEqualToString:@""]
+}
+*/
+
+@end
